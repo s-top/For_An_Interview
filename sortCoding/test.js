d5295973a21078c7db49e6a418187f78bbf31b20
s-top <15634647@qq.com>
2018-09-15 23:00:13 +0800

    2018-09-12

---
+++ sortCoding/test.js
@@ -0,0 +1,12 @@
+
+var line = ["",0,{"name":"sina"},[1,2],"me","15.8",null,{}];
+
+function a(line) {
+    for(i in line){
+        console.log(line[i]);
+        // line.replace(i," ");
+    }
+    // print(line);
+}
+
+a(line);
